fc88b6abc2e67c64f4697562add373e65f4dd9a4
trustmitt <mainmirror14@gmail.com>
2023-03-15 22:59:41 +0900

    chore : 초기 환경 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useState } from 'react';
+import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { RecoilRoot } from 'recoil';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from '@/styles/globalStyle';
+import { theme } from '@/styles/theme';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </RecoilRoot>
+    </QueryClientProvider>
+  );
 }
+
+export default MyApp;
---
+++ styles/theme.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+  colors: {
+    primary: '#7CB896',
+    secondary: '#9CC8A4',
+    primaryDark: '#48A684',
+    primaryLight: '#B6D4AE',
+    textColor: '#343A40',
+    bgColor: '#F8F9FA',
+  },
+};
